fec0fc27e8a0b72c4c62d072e969a4e4fdbbea58
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2021-12-16 18:26:19 +0800

    Rename 02.c to 冒泡排序.c

--- 冒泡排序.c
+++ 冒泡排序.c
@@ -7,7 +7,7 @@ int main()
     int a[100];
     for (i = 0; i < n; i++)
         scanf("%d", &a[i]);
-    for (i = 0; i < n - 1; i++)            //ºËÐÄÅÅÐò
+    for (i = 0; i < n - 1; i++)            //ÂºÃ‹ÃÃ„Ã…Ã…ÃÃ²
         for (j = 0; j < n - i - 1; j++)
             if (a[j] < a[j + 1]) {
                 t = a[j]; a[j] = a[j + 1]; a[j + 1] = t;

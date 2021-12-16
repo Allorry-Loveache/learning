94dde3603fdfd598f3ec19825b1e597931f0b6f6
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2021-12-16 18:44:23 +0800

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+int factorsum(int number) {  //求一个数的因子和
+	int sum = 1;
+	if (number == 1) return 0;
+	for (int i = 2; i <= number / i; i++)
+		if (number % i == 0) {
+			sum += i;
+			if (i != number / i)
+				sum += number / i;
+		}
+	return sum;
+}
+void PrintPN(int m, int n) {
+	int flag = 0, i, j;
+	for (i = m; i <= n; i++) {
+		if (i == factorsum(i)) {
+			flag = 1;
+			printf("%d = 1", i);
+			for (j = 2; j <= i / 2; j++)
+				if (i % j == 0)
+					printf(" + %d", j);
+			printf("\n");
+		}
+	}
+	if (!flag) printf("No perfect number");
+}

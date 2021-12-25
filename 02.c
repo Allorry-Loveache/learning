4ecbfa32a54ece871fbae17fd94c5e2061f4e34b
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2021-12-25 11:54:16 +0800

    Add files via upload

--- 02.c
+++ 02.c
@@ -1,27 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#define MAXN 20
-void delchar(char* str, char c);
-void ReadString(char s[]); /* 由裁判实现，略去不表 */
+#include<stdio.h>
 int main()
 {
-    char str[MAXN], c;
-    scanf("%c\n", &c);
-    ReadString(str);
-    delchar(str, c);
-    printf("%s\n", str);
-    return 0;
+	int n, m;
+	scanf("%d %d", &m, &n);
+	n--;
+	int t = m;
+	while (n--) t *= m;
+	printf("%03d", t % 1000);
 }
-void delchar(char* str, char c) {
-    char* t = str;
-    while (*str != '\0') {
-        if (*str == c)
-            str++;
-        else {
-            *t = *str;
-            str++; t++;
-        }
-    }
-    *t = '\0';
-}
-//a   b b b a a a b 
+ 

06bd13ba19dde3efeb22d47e63d39e2861623d3d
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-03-17 19:13:14 +0800

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+string a;
+string b[27] = { "#","one","two","three","four",
+"five","six","seven","eight","nine","ten","eleven",
+"twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+"seventeen", "eighteen", "nineteen", "twenty"," a ",   //这个a挺烦的
+"both","another","first","second","third" };//对应
+int d[30] = { 0,1,4,9,16,25,36,49,64,81,00,21,44,69,96,25,56,89,24,61,0,1,4,1,1,4,9 };
+int c[10];
+int main()
+{
+	int i, j = 0, flag = 1;
+	getline(cin, a);
+	for (i = 0; i < 27; i++) {
+		if (a.find(b[i]) < a.length()) {
+			c[j++] = d[i];
+		}
+	}
+	if (j == 0) {
+		printf("0");
+		return 0;
+	}
+	sort(c, c + j);
+	for (i = 0; i < j; i++) {
+		if (flag) {
+			if (c[i] == 0) continue;
+			printf("%d", c[i]);
+			flag = 0;
+		}
+		else {
+			printf("%02d", c[i]);
+		}
+	}
+}

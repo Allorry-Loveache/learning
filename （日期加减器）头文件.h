50c115a0f554e4fafd572dd3f99b3158dbcbe4c4
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-03-29 16:02:56 +0800

    Update （日期加减器）头文件.h

--- （日期加减器）头文件.h
+++ （日期加减器）头文件.h
@@ -1,8 +1,8 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-using std::cout;
-using std::cin;
-using std::endl;
+#include<cstdio>
+using namespace std;
 
 class Date {
 private:
@@ -10,10 +10,15 @@ private:
 	int _month;
 	int _day;
 public:
+	void menu();
 	Date(int year=2024, int month=3, int day=7);
+	void set(int& year, int& month, int& day);
 	Date& operator=(const Date& d);
 	void operator+=(int x);
 	void operator-=(int x);
 	void Print();
+	friend ostream& operator<<(ostream& out, const Date& d);
+	friend istream& operator>>(istream& in, Date& d);
+	int cmp(int& year, int& month, int& day);
+	bool judge(int& year, int& month, int& day);
 };
-

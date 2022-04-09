6d01e579dd57b99e9c0e5bf40f5819628ecbd60a
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-04-09 15:01:15 +0800

    Update and rename 01.cpp to （数学）统计方形.cpp

---
+++ （数学）统计方形.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<string.h>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+long long  n, m, rec, sqr;
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)//循环，从n-0到n-(n-1)
+        for (int j = 0; j < m; j++) {//循环，从m-0到m-(m-1)
+            if (i == j) sqr += (n - i) * (m - j);//如果i==j，说明是正方形
+            else rec += (n - i) * (m - j);//如果不等说明是矩形
+        }
+    cout << sqr << " " << rec << endl;//输出
+    return 0;
+}

8097099292ce7434a98923e941b0f3f5d6df6e1a
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-05-01 11:36:22 +0800

    Update （String 类）模拟实现.h

--- （String 类）模拟实现.h
+++ （String 类）模拟实现.h
@@ -1,13 +1,13 @@
-#pragma once
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-#include<algorithm>
 #include<assert.h>
+#include<algorithm>
 using namespace std;
+
+
 namespace allorry {
+
 	class string {
-		//管理字符串的数组，可以增删查改
-		//结尾带有'\0'
 	private:
 		char* _str;
 		size_t _size;
@@ -20,107 +20,91 @@ namespace allorry {
 		iterator end() const{
 			return _str + _size;
 		}
-		void swap(string& s) {  //类内swap与模板swap合并
+		void swap(string& s) {
 			::swap(_str, s._str);
-			::swap(_size, s._size);
 			::swap(_capacity, s._capacity);
+			::swap(_size, s._size);
 		}
-		string(const char* str = "") {
-			_capacity = _size = strlen(str);
-			_str = new char[_capacity + 1]; 
-			//空间多开一个存'\0'，后续也稍微注意些
-			strcpy(_str, str);
+		string(const char* t="") {
+			_capacity = _size = strlen(t);
+			_str = new char[_size + 1];
+			strcpy(_str, t);
 		}
-		string(const string& s):_str(nullptr)
-		{    //不把_str置空，析构t随机值的时候会出现问题
-			string t(s._str); //构造函数打造替代品
+		string(const string& s) :_str(nullptr) {
+			string t(s._str);
 			swap(t);
 		}
 		~string() {
-			delete[] _str, _str = nullptr;
+			delete[] _str;
+			_str = nullptr;
+			_capacity = _size = 0;
 		}
-		size_t size() {
+		size_t size() const{
 			return _size;
 		}
-		void reserve(int n) { //开辟新空间
-			n = (n == 0) ? 1 : n;  //开辟0个空间会坏事
+		size_t capacity() const{
+			return _capacity;
+		}
+		void reserve(size_t n) {
+			n = n == 0 ? 1 : n;
 			if (n <= _capacity) return;
-			char* t = new char[n + 1]; //多开，'\0'
+
+			char* t = new char[n + 1];
 			strcpy(t, _str);
-			delete[] _str;
-			_str = t;
+			delete[] _str; _str = t;
 			_capacity = n;
 		}
-		void resize(size_t n, char val='\0') {
-	    //假设size=4，capacity=8，二者分了三种情况，一三种需要讨论
-			if (n < _size) {
-				_size = n;
-				_str[_size] = '\0';
-			}
+		void resize(size_t n, char ch = '\0') {
+			assert(n >= 0);
+			if (n < _size) return;   //别缩小了，不好玩
 			else {
-				if (n > _capacity) 
-					reserve(n);
-				for (int i = _size; i < n; i++)
-					_str[i] = val;
-				_str[n] = '\0';
+				if (n > _capacity) reserve(n);
+				for (int i = _size; i < n; i++) 
+					_str[i] = ch;
 				_size = n;
+				_str[n] = '\0';
 			}
 		}
-		void push_back(char ch) {
-			if (_size == _capacity) 
-				reserve(_capacity * 2);
-			_str[_size] = ch;
-			_str[_size + 1] = '\0';
-			_size++;
-		}
-		void insert(size_t pos, char ch) {
-			//坑点：size_t n=0, n-1 > 4e10
+		void insert(size_t pos, const char* s) {
 			assert(pos <= _size);
-			if (_size == _capacity)
-				reserve(2 * _capacity);
-			char* end = _str + _size;
-			while (end >= _str + pos) {
-				*(end + 1) = *(end); end--;
+			size_t len = strlen(s);
+			if (len + _size > _capacity) reserve(len + _size);
+
+			char* tail = _str + _size;
+			while (tail != _str + pos) {
+				*(tail + len) = *tail;
+				tail--;
 			}
-			_str[pos] = ch;
-			_size++;
-		}
-		void insert(size_t pos, const char* str) {
-			assert(pos <= _size);
-			size_t len = strlen(str);
-			if (len + _size > _capacity)
-				reserve(len + _size);
-			char* end = _str + _size;
-			while (end >= _str + pos) {
-				*(end + len) = *(end); end--;
-			}
-			strncpy(_str + pos, str, len); //防止str的\0覆盖有效数据
+			strncpy(_str + pos, s, len);
 			_size += len;
 		}
-		void append(const char* str) {
-			/*size_t len = _size + strlen(str);
-			if (len > _capacity)
-				reserve(len);
-			strcpy(_str + _size, str);
-			_size = len;*/
-
-			insert(_size, str);
+		void check() {
+			if (_size == _capacity) reserve(_capacity * 2);
 		}
-		size_t find(char ch, size_t pos=0) {
-			for (size_t i = pos; i < _size; i++)
-				if (ch == _str[i]) return i;
+		void push_back(const char ch) {
+			check();
+			_str[_size] = ch;
+			_str[++_size] = '\0';
+		}
+		void append(const char* s) {
+			insert(_size, s);
+		}
+		size_t find(const char ch, size_t pos) {
+			for (int i = pos; i < _size; i++) {
+				if (_str[i] == ch) return i;
+			}
 			return -1;
 		}
-		size_t find(const char* str, size_t pos=0) {
-			const char* ret = strstr(_str + pos, str);
-			if (ret) return ret - _str;
-			else return -1;
+		size_t find(const char* s, size_t pos) {
+			const char* t = strstr(_str + pos, s);
+			if (t) return t - _str;
+			return -1;
 		}
 		void erase(size_t pos, size_t len) {
 			assert(pos < _size);
-			if (len + pos >= _size) { //全部删完
-				_str[pos] = '/0';
-				_size -= len;
+			if (len + pos > _size) {
+				_str[pos] = '\0';
+				_size = pos + 1;
 			}
 			else {
 				strcpy(_str + pos, _str + pos + len);
@@ -144,13 +128,13 @@ namespace allorry {
 			return *this;
 		}
 		bool operator<(const string& s) {
-			return strcmp(c_str(), s.c_str()) < 0;
+			return strcmp(_str, s._str) < 0;
 		}
 		bool operator>(const string& s) {
-			return strcmp(c_str(), s.c_str()) > 0;
+			return strcmp(_str, s._str) > 0;
 		}
 		bool operator==(const string& s) {
-			return strcmp(c_str(), s.c_str())==0;
+			return strcmp(_str, s._str) == 0;
 		}
 		bool operator<=(const string& s) {
 			return *this == s || *this < s;
@@ -158,15 +142,12 @@ namespace allorry {
 		bool operator>=(const string& s) {
 			return *this == s || *this > s;
 		}
-		char& operator[](int i) const{ //返回引用
+		char& operator[](int i) const{
 			assert(i < _size);
 			return _str[i];
 		}
-		char* c_str() const {
-			return _str;
-		}
 		void everyone() {
-			for (iterator i = this->begin(); i < this->end(); i++) {
+			for (iterator i = begin(); i < end(); i++) {
 				cout << *i << ' ';
 			}
 		}
@@ -174,32 +155,44 @@ namespace allorry {
 			_size = 0;
 			_str[0] = '\0';
 		}
-	};
-	ostream& operator<<(ostream& out, const string& s) {
-		//out << s.c_str();
-		for (auto ch : s) out << ch;
-		return out;
-	}
-	istream& operator>>(istream& in, string& s) {
-		s.clear();
-		char ch;
-		//in >> ch;  //忽略' '等间隔符， 导致一直循环
-		ch = in.get();
-		while (ch!=' ' && ch!= '\r' && ch!='\n') {
-			s += ch;
-			ch=in.get();
-		}
-		return in;
-	}
-	istream& getline(istream& in, string& s) {
-		s.clear();
-		char ch;
-		ch = in.get();
-		while (ch != '\r' && ch != '\n') {
-			s += ch;
-			ch = in.get();
-		}
-		return in;
-	}
-}
+		//错误示例：直接按照固有思维输入，没有考虑增容，直接暴毙
+		/*friend istream& operator>>(istream& in, const string& s) {
+			in >> s._str;
+			return in;
+		}*/
 
+		friend istream& operator>>(istream& in, string& s) {
+			s.clear();
+			char ch;
+			//in >> ch;  //忽略' '等间隔符， 导致一直循环
+			ch = in.get();
+			while (ch != ' ' && ch != '\r' && ch != '\n') {
+				s += ch;
+				ch = in.get();
+			}
+			return in;
+		}
+
+		//错误示例：按照string的思维输出，但模拟的时候要用char*的思维
+		/*friend ostream& operator<<(ostream& out, const string& s) {
+			out << s._str;
+			return out;
+		}*/
+		friend ostream& operator<<(ostream& out, const string& s) {
+			//out << s.c_str();
+			for (auto ch : s) out << ch;
+			return out;
+		}
+
+		istream& getline(istream& in, string& s) {   //差别只在getline可以输入空格
+			s.clear();
+			char ch;
+			ch = in.get();
+			while (ch != '\r' && ch != '\n') {
+				s += ch;
+				ch = in.get();
+			}
+			return in;
+		}
+	};
+}

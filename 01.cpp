7fc4b964b4ef469758c65ca3403ab71e6adb049b
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-05-03 09:38:18 +0800

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class haoye {
+	int x, y, z;
+public:
+	haoye(int a = 1, int b = 1, int c = 1) :x(a), y(b), z(c) {}
+	void surface() {
+		cout << 2 * (x * y + y * z + z * x) << " ";
+	}
+	void volume() {
+		cout << x * y * z << endl;
+	}
+	void judge(const haoye& t) {
+		if (x == t.x && y == t.y) {
+			z *= 2;
+			surface();
+			volume();
+			z /= 2;
+		}
+		if (y == t.y && z == t.z) {
+			x *= 2;
+			surface();
+			volume();
+			x /= 2;
+		}
+		if (z == t.z && x == t.x) {
+			y *= 2;
+			surface();
+			volume();
+			y /= 2;
+		}
+	}
+};
+int main()
+{
+	haoye a(1, 2, 3), b(1, 2, 3);
+	a.judge(b);
+}

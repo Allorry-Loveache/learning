e1a949b9dfe27e960eef61060b729cab8132989f
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-06-07 19:56:07 +0800

    Update （Vector）模式实现.h

--- （Vector）模式实现.h
+++ （Vector）模式实现.h
@@ -1,14 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<assert.h>
-#include<algorithm>
 using namespace std;
 namespace allorry {
 	template <class T>
 	class vector {
 	public:
 		typedef T* iterator;
-		void swap(T& x) {
+		void swap(vector<T>& x) {
 			::swap(_start, x._start);
 			::swap(_end, x._end);
 			::swap(_finish, x._finish);
@@ -20,9 +19,18 @@ namespace allorry {
 			return _finish;
 		}
 		vector() :_start(nullptr), _finish(nullptr), _end(nullptr) {}
-		vector(vector<T>& x):_start(nullptr), _finish(nullptr), _end(nullptr) {
-			vector <T> t(x.begin(), x.end());
-			swap(x);
+		vector(size_t x) :_start(nullptr), _finish(nullptr), _end(nullptr) {
+			resize(x);
+		}
+		vector(iterator l, iterator r) :_start(nullptr), _finish(nullptr), _end(nullptr) {
+			size_t sz = r - l;
+			T* t = new T[sz];
+			memcpy(t, l, sz*sizeof(T));
+			_start = t, _end = _finish = _start + sz;
+		}
+		vector(vector<T>& x) :_start(nullptr), _finish(nullptr), _end(nullptr) {
+			vector<T> t(x.begin(), x.end());
+			swap(t);
 		}
 		~vector() {
 			if (_start) delete[] _start;
@@ -39,7 +47,7 @@ namespace allorry {
 				T* t = new T[n];
 				size_t sz = size(); //先保留好原空间的大小
 				if (_start) { //原空间有东西的话就拷过去，注意此处摧毁空间
-					memcpy(t, _start, sz*sizeof(T));
+					memcpy(t, _start, sz * sizeof(T));
 					delete[] _start;
 				}
 				//以上运用memcpy只适用于浅拷贝，以下通用
@@ -60,7 +68,7 @@ namespace allorry {
 			*_finish = x;
 			_finish++;
 		}
-		void resize(size_t n, T val=T()) {  //新的容量 与 填补值
+		void resize(size_t n, T val = T()) {  //新的容量 与 填补值
 			if (n < size()) {
 				_finish = _start + n;
 			}
@@ -91,14 +99,14 @@ namespace allorry {
 			}
 			*pos = x;
 			_finish++;
-		/*面试问：这里的pos是形参，而原先的实参已经因为扩容失效了，
-		为了避免这个问题，你该怎么做？*/
+			/*面试问：这里的pos是形参，而原先的实参已经因为扩容失效了，
+			为了避免这个问题，你该怎么做？*/
 			//改为：iterator& pos,  末尾添加：pos++;
 		}
 		iterator erase(iterator pos) {
 			iterator cur = pos;
 			while (cur != _finish) {
-				*cur = *(cur+1);
+				*cur = *(cur + 1);
 				cur++;
 			}
 			_finish--;
@@ -112,6 +120,13 @@ namespace allorry {
 			swap(t);
 			return *this;
 		}
+		friend ostream& operator<<(ostream& out,vector<T>& x) {
+			for (int i = 0; i < x.size(); i++) {
+				out << x[i] << ' ';
+			}
+			out << endl;
+			return out;
+		}
 	private:
 		iterator _start;
 		iterator _finish;
@@ -120,5 +135,12 @@ namespace allorry {
 
 }
 
-
-
+int main()
+{
+	allorry::vector<int> a(10);
+	for (int i = 0; i < 10; ++i)  a[i] = i * i;
+	allorry::vector<int> b(20);
+	b = a;
+	cout << a;
+	cout << b;
+}

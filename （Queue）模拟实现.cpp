be3d5ed1bf39949d6459754703c212669ddf1592
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-06-07 14:27:56 +0800

    Update （Queue）模拟实现.cpp

--- （Queue）模拟实现.cpp
+++ （Queue）模拟实现.cpp
@@ -1,70 +1,40 @@
-#include<iostream>
-using namespace std;
-#include<deque>
-
-//库函数stack的底层结构为deque
-//模板参数: T--数据类型    Container--底层数据结构
-template<class T, class Container = deque<T>>
-class stack
-{
-public:
-	stack()
-		:con()
-	{}
-
-	void push(const T& x)
-	{
-		con.push_back(x);
-	}
-	void pop()
-	{
-		con.pop_back();
-	}
-	size_t size()
-	{
-		return con.size();
-	}
-	T& top()
-	{
-		return con.back();
-	}
-	const T& top()const
-	{
-		return con.back();
-	}
-	bool empty()
-	{
-		return con.empty();
-	}
-private:
-	Container con;
-};
-
-void testStack()
-{
-	stack<int> s;
-	if (s.empty())
-	{
-		cout << s.size() << endl;
-	}
-	s.push(5);
-	s.push(4);
-	s.push(3);
-	s.push(2);
-	s.push(1);
-	cout << s.size() << endl;
-
-	cout << s.top() << endl;
-
-	s.pop();
-	s.pop();
-	cout << s.size() << endl;
-	cout << s.top() << endl;
-
-}
-
-int main()
-{
-	testStack();
-	return 0;
-}
+template<class T, class Con = deque<T>>
+    class queue
+    {
+        public:
+            queue();
+            void push(const T& x)
+            {
+                _c.push_back(x);
+            }
+            void pop()
+            {
+                _c.pop_front();
+            }
+            T& back()
+            {
+                return _c.back();
+            }
+            const T& back()const
+            {
+                return _c.back();
+            }
+            T& front()
+            {
+                return _c.front();
+            }
+            const T& front()const
+            {
+                return _c.front();
+            }
+            size_t size()const
+            {
+                return _c.size();
+            }
+            bool empty()const
+            {
+                return _c.empty();
+            }
+        private:
+            Con _c;
+    };

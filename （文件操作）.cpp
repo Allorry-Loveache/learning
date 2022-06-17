ca211c4b512ce78c9fc2552f408d88d0ac077368
Allorry-Loveache <95964237+Allorry-Loveache@users.noreply.github.com>
2022-06-17 10:59:22 +0800

    Update （文件操作）.cpp

--- （文件操作）.cpp
+++ （文件操作）.cpp
@@ -5,64 +5,64 @@
 
 
 
-//第一题
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<bits/stdc++.h>
-//using namespace std;
-//int main()
-//{
-//
-//	FILE* p = fopen("text.txt", "r");
-//	char t[100], ch;
-//	int sum = 0, flag=1;
-//	while (!feof(p)) {
-//		ch = fgetc(p);
-//		if (ch == '\n') flag = 0;
-//		else {
-//			if (flag == 0) sum++, flag = 1;
-//		}
-//		if (ch >= 65 && ch <= 90) ch += 32;
-//		cout << ch;
-//	}
-//	cout << endl << sum;
-//}
+第一题
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	FILE* p = fopen("text.txt", "r");
+	char t[100], ch;
+	int sum = 0, flag=1;
+	while (!feof(p)) {
+		ch = fgetc(p);
+		if (ch == '\n') flag = 0;
+		else {
+			if (flag == 0) sum++, flag = 1;
+		}
+		if (ch >= 65 && ch <= 90) ch += 32;
+		cout << ch;
+	}
+	cout << endl << sum;
+}
 
 
-//第三题
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<bits/stdc++.h>
-//using namespace std;
-//int main()
-//{
-//
-//	FILE* p = fopen("text2.txt", "r");
-//	char t[100], ch;
-//	int count = 0;
-//	int flag = 1;
-//	int sum = 0;
-//	while (!feof(p)) {
-//		ch = fgetc(p);
-//		if (ch >= '0' && ch <= '9')
-//		{
-//			count = count * 10 + ch - '0';
-//		}
-//		else {
-//			sum += count;
-//			count = 0;
-//		}
-//	}
-//	p = fopen("text2.txt", "a+");
-//	fprintf(p, "%d", sum);
-//	fclose(p);
-//	
-//	cout << endl << sum;
-//}
+第三题
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	FILE* p = fopen("text2.txt", "r");
+	char t[100], ch;
+	int count = 0;
+	int flag = 1;
+	int sum = 0;
+	while (!feof(p)) {
+		ch = fgetc(p);
+		if (ch >= '0' && ch <= '9')
+		{
+			count = count * 10 + ch - '0';
+		}
+		else {
+			sum += count;
+			count = 0;
+		}
+	}
+	p = fopen("text2.txt", "a+");
+	fprintf(p, "%d", sum);
+	fclose(p);
+	
+	cout << endl << sum;
+}
 
 
 
-//第二题
-//本题一个文件里放本来的密码
-//我把加密后的文件放到了另一个文件里
+第二题
+本题一个文件里放本来的密码
+我把加密后的文件放到了另一个文件里
 #define _CRT_SECURE_NO_WARNINGS
 #include<bits/stdc++.h>
 using namespace std;
